577a387e0e2f86d276a5649ecd1c269b88794a05
pablocarvalho <pablomccarvalho@gmail.com>
2019-10-30 00:00:10 -0300

    adding feature importance with grid search example to run script

--- run_example.sh
+++ run_example.sh
@@ -5,3 +5,12 @@
 #Execution model for tesla p100 data with metrics output
 ./stratified_classification_methods.py -i data/tesla_p100_attempts.csv -v -m attempts 2>&1 | tee data/p100_concurrency_verbose_metrics.txt
 ./stratified_classification_methods.py -i data/tesla_p100_CE.csv -v -m ce 2>&1 | tee data/p100_interference_verbose_metrics.txt
+
+
+#Execution model for rtx 2080 data with metrics output and grid-search
+./stratified_classification_methods.py -i data/rtx-2080_attempts.csv -v -g -m attempts 2>&1 | tee data/rtx-2080_concurrency_verbose_metrics_grid.txt
+./stratified_classification_methods.py -i data/rtx-2080_CE.csv -v -g -m ce 2>&1 | tee data/rtx-2080_interference_verbose_metrics_grid.txt
+
+#Execution model for tesla p100 data with metrics output and grid-search
+./stratified_classification_methods.py -i data/tesla_p100_attempts.csv -v -g -m attempts 2>&1 | tee data/p100_concurrency_verbose_metrics_grid.txt
+./stratified_classification_methods.py -i data/tesla_p100_CE.csv -v -g -m ce 2>&1 | tee data/p100_interference_verbose_metrics_grid.txt
